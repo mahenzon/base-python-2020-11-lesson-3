6f36543f7dddf04e727e52af1e2317db663928ae
Suren Khorenyan <surenkhorenyan@gmail.com>
2020-12-05 11:57:43 +0300

    add generators demo

--- main.py
+++ main.py
@@ -282,3 +282,42 @@ def lesson2():
     print("n", next(iterator))
     print("n", next(iterator))
     # print("n", next(iterator))
+
+
+# generators
+
+def pow(up_to, e=2):
+    results = []
+    for i in range(up_to):
+        results.append(i ** e)
+    return results
+
+
+print("pow up to 13", pow(13))
+
+
+def pow_gen(up_to, e=3):
+    print("in pow gen")
+    for i in range(up_to):
+        print("process", i)
+        yield i ** e
+        print("continue from", i)
+
+
+print("pow_gen up to 13", list(pow_gen(13)))
+
+for i in pow_gen(13):
+    print(i)
+    if i > 100:
+        print("i is gt 100")
+        break
+
+
+def true_and_false(val=True):
+    while True:
+        yield val
+        val = not val
+
+g = true_and_false()
+for i in range(100):
+    print(next(g))
